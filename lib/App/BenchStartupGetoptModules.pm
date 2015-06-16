f235be2b906b2e9aebd952db2c09e922c473ea1b
perlancar (@pc-office) <perlancar@gmail.com>
2015-06-16 15:07:48 +0700

    Initial commit

---
+++ lib/App/BenchStartupGetoptModules.pm
@@ -0,0 +1,11 @@
+package App::BenchStartupGetoptModules;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Benchmark startup time of some popular Perl Getopt modules
+
+=head1 SYNOPSIS
+
+See the included script L<bench-startup-getopt-modules>.
